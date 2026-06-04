2f688db2f2c4443790445a4ed964f18900ed9f8e
tonystark037 <atharvamhamane37@gmail.com>
2026-06-04 11:23:05 +0530

    Update messages.js

--- messages/messages.js
+++ messages/messages.js
@@ -1,1 +1,52 @@
+const SUPABASE_URL =
+"https://zzmceyjguctywocofjun.supabase.co";
 
+const SUPABASE_KEY =
+"sb_publishable_bgGrGBdrGUAfsQiujIqNmg_-cF56igy";
+
+const db =
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
+async function saveMessage() {
+
+const title =
+document.getElementById("messageTitle").value;
+
+const text =
+document.getElementById("messageText").value;
+
+const side =
+document.getElementById("messageSide").value;
+
+const { error } =
+await db
+.from("messages")
+.insert([
+{
+title,
+text,
+side
+}
+]);
+
+if(error){
+
+console.error(error);
+alert("Save failed");
+
+}else{
+
+alert("Saved successfully ❤️");
+
+}
+
+}
+
+function renderMessages(){}
+
+async function loadMessages(){}
+
+loadMessages();
