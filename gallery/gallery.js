0dbac990561b116725db293e8881c98426a89d80
tonystark037 <atharvamhamane37@gmail.com>
2026-06-05 00:17:51 +0530

    Update gallery.js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -72,7 +72,7 @@ file.name.replaceAll(
 const {
 error: uploadError
 } = await db.storage
-.from("gallery")
+.from("GALLERY")
 .upload(
 fileName,
 file
@@ -97,7 +97,7 @@ return;
 const {
 data
 } = db.storage
-.from("gallery")
+.from("GALLERY")
 .getPublicUrl(
 fileName
 );
@@ -110,7 +110,7 @@ data.publicUrl;
 const {
 error
 } = await db
-.from("gallery")
+.from("GALLERY")
 .insert([{
 
 title,
@@ -168,7 +168,7 @@ const {
 data,
 error
 } = await db
-.from("gallery")
+.from("GALLERY")
 .select("*")
 .order(
 "created_at",
@@ -256,7 +256,7 @@ if(
 const {
 error
 } = await db
-.from("gallery")
+.from("GALLERY")
 .delete()
 .eq(
 "id",
